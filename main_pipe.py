b2278cacaa923a17c487ed459d5cda600c0f6dbd
AyoDennis <ayodejidennis33@gmail.com>
2024-07-18 21:05:02 +0100

    parameters

---
+++ main_pipe.py
@@ -0,0 +1,18 @@
+# Snowflake connection parameters
+snowflake_user = 'your_snowflake_user'
+snowflake_password = 'your_snowflake_password'
+snowflake_account = 'your_snowflake_account'
+snowflake_warehouse = 'your_snowflake_warehouse'
+snowflake_database = 'your_snowflake_database'
+snowflake_schema = 'your_snowflake_schema'
+
+# Connection
+conn = snowfl.connector.connect(
+    user=snowflake_user,
+    password=snowflake_password,
+    account=snowflake_account,
+    warehouse=snowflake_warehouse,
+    database=snowflake_database,
+    schema=snowflake_schema
+)
+
