1336a29bf69ecdc5c76658e82f9618ab0203f804
AyoDennis <ayodejidennis33@gmail.com>
2024-07-19 22:29:33 +0100

    manual load

---
+++ manual_load.py
@@ -0,0 +1,81 @@
+import snowflake.connector
+from snowflake.connector import DictCursor
+
+# Snowflake connection parameters
+snowflake_user = 'your_snowflake_user'
+snowflake_password = 'your_snowflake_password'
+snowflake_account = 'your_snowflake_account'
+snowflake_warehouse = 'your_snowflake_warehouse'
+snowflake_database = 'your_snowflake_database'
+snowflake_schema = 'your_snowflake_schema'
+
+# Connection
+conn = snowflake.connector.connect(
+    user=snowflake_user,
+    password=snowflake_password,
+    account=snowflake_account,
+    warehouse=snowflake_warehouse,
+    database=snowflake_database,
+    schema=snowflake_schema
+)
+
+
+# Create a cursor object using DictCursor for easier dictionary handling
+cur = conn.cursor(DictCursor)
+
+
+# Create an external stage pointing to your S3 bucket
+sql_create_stage = """
+    CREATE OR REPLACE STAGE stage_name
+    URL = 's3://name_of_s3_bucket/'
+    CREDENTIALS = (
+      AWS_KEY_ID = 'paste_from_your_credential',
+      AWS_SECRET_KEY = 'paste_from_your_credential'
+    )
+    """
+cur.execute(sql_create_stage)
+print("External stage created successfully.")
+
+
+# Create a file format for CSV files
+sql_create_file_format = """
+    CREATE OR REPLACE FILE FORMAT file_format
+    TYPE = 'TYPE_OF_FILE(e.g CSV)'
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    SKIP_HEADER = 1
+    FIELD_DELIMITER = ','
+    """
+cur.execute(sql_create_file_format)
+print("File format created successfully.")
+
+# Create a table with a flexible schema
+sql_create_table = """
+    CREATE OR REPLACE TABLE table_name
+    USING TEMPLATE (
+    SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
+    FROM TABLE(
+        INFER_SCHEMA(
+            LOCATION => '@stage_name/file_prefix/file_name.csv',
+            FILE_FORMAT => 'csv_format'
+        )
+    )
+);
+
+    """
+cur.execute(sql_create_table)
+print("Table with flexible schema created successfully.")
+
+    # Test manual data loading from S3 to Snowflake table
+sql_copy_into = """
+    COPY INTO sales_table
+    FROM @stage_name/file_prefix/file_name.csv
+    FILE_FORMAT = (FORMAT_NAME = csv_format)
+    MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+
+    """
+cur.execute(sql_copy_into)
+print("Data manually loaded successfully.")
+
+# Close cursor and connection
+cur.close()
+conn.close()
