c3fb742a233279200ac25385b38878bd1dc91634
AyoDennis <ayodejidennis33@gmail.com>
2024-07-19 13:26:34 +0100

    Create a Snowpipe to automatically ingest data into the table

--- main_pipe.py
+++ main_pipe.py
@@ -26,11 +26,11 @@ cur = conn.cursor(DictCursor)
 
 # Create an external stage pointing to your S3 bucket
 sql_create_stage = """
-    CREATE OR REPLACE STAGE s3_stage
-    URL = 's3://name-of-s3-bucket/'
+    CREATE OR REPLACE STAGE stage_name
+    URL = 's3://name_of_s3_bucket/'
     CREDENTIALS = (
-      AWS_KEY_ID = 'paste-from-your-credential',
-      AWS_SECRET_KEY = 'paste-from-your-credential'
+      AWS_KEY_ID = 'paste_from_your_credential',
+      AWS_SECRET_KEY = 'paste_from_your_credential'
     )
     """
 cur.execute(sql_create_stage)
@@ -39,8 +39,8 @@ print("External stage created successfully.")
 
 # Create a file format for CSV files
 sql_create_file_format = """
-    CREATE OR REPLACE FILE FORMAT csv_format
-    TYPE = 'CSV'
+    CREATE OR REPLACE FILE FORMAT file_format
+    TYPE = 'TYPE_OF_FILE(e.g CSV)'
     FIELD_OPTIONALLY_ENCLOSED_BY = '"'
     SKIP_HEADER = 1
     FIELD_DELIMITER = ','
@@ -51,7 +51,7 @@ print("File format created successfully.")
 
 # Create a table with a flexible schema
 sql_create_table = """
-    CREATE OR REPLACE TABLE sales_table
+    CREATE OR REPLACE TABLE table_name
     (
         VARIANT_COLUMN VARIANT
     );
@@ -59,12 +59,24 @@ sql_create_table = """
 cur.execute(sql_create_table)
 print("Table with flexible schema created successfully.")
 
-  # Test manual data loading from S3 to Snowflake table
+# Test manual data loading from S3 to Snowflake table
 sql_copy_into = """
-    COPY INTO sales_table
-    FROM @s3_stage/sftp_files/sales_file.csv
+    COPY INTO table_table
+    FROM @s3_stage/s3_folder_name/file_name.csv
     FILE_FORMAT = (FORMAT_NAME = 'csv_format')
     ON_ERROR = 'CONTINUE';
     """
 cur.execute(sql_copy_into)
 print("Data manually loaded successfully.")
+
+# Create a Snowpipe to automatically ingest data into the table
+sql_create_pipe = """
+CREATE OR REPLACE PIPE NAME_OF_YOUR-PIPE
+AUTO_INGEST = TRUE
+AS
+COPY INTO YOUR_DATABASE_NAME.SCHEMA_NAME.table_name
+FROM @stage_name/s3_folder/s3_file.format
+FILE_FORMAT = (FORMAT_NAME = 'csv_format')
+ON_ERROR = 'CONTINUE';
+
+"""
